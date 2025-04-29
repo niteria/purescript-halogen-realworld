6008dcbc93d46f88a9055876437fe31827337bd4
Bartosz Nitka <niteria@gmail.com>
2025-04-29 20:55:02 +0200

    Build with mkSpagoDerivation

--- flake.nix
+++ flake.nix
@@ -1,51 +1,45 @@
 {
-  description = "RealWorld spec in the PureScript Halogen framework";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mkSpagoDerivation.url = "github:jeslie0/mkSpagoDerivation";
+    ps-overlay.url = "github:thomashoneyman/purescript-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    purescript-overlay,
-    ...
-  }: let
-    name = "halogen-realworld";
-
-    supportedSystems = [
-      "aarch64-darwin"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-
-    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-  in {
-    devShell = forAllSystems (system: let
-      overlays = [purescript-overlay.overlays.default];
-      pkgs = import nixpkgs {inherit system overlays;};
-    in
-      pkgs.mkShell {
-        inherit name;
-        buildInputs =
-          [
-            pkgs.esbuild
-            pkgs.nodejs_20
-            pkgs.nixpkgs-fmt
-            pkgs.purs
-            pkgs.purs-tidy
-            pkgs.purs-backend-es
-            pkgs.purescript-language-server
-            pkgs.spago-unstable
-          ]
-          ++ (pkgs.lib.optionals (system == "aarch64-darwin")
-            (with pkgs.darwin.apple_sdk.frameworks; [
-              Cocoa
-              CoreServices
-            ]));
-      });
-  };
+  outputs = { self, nixpkgs, flake-utils, mkSpagoDerivation, ps-overlay }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ mkSpagoDerivation.overlays.default
+                       ps-overlay.overlays.default ];
+        };
+        nodeDependencies = (pkgs.callPackage ./node/default.nix { }).nodeDependencies;
+      in
+        {
+          packages.default =
+            pkgs.mkSpagoDerivation {
+              spagoYaml = ./spago.yaml;
+              spagoLock = ./spago.lock;
+              src = ./.;
+              nativeBuildInputs = [ pkgs.purs-unstable pkgs.spago-unstable pkgs.esbuild pkgs.purescript-language-server pkgs.node2nix];
+              version = "0.1.0";
+              buildPhase = ''
+                ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+                spago bundle
+              '';
+              installPhase = ''
+                mkdir $out;
+                cp ${./dist/index.html} $out/index.html;
+                cp index.js $out/index.js;
+              '';
+              shellHook = ''
+                if [ ! -L "./node_modules" ]; then
+                  ln -s "${nodeDependencies}/lib/node_modules" ./node_modules
+                fi
+              '';
+            };
+        }
+    );
 }
