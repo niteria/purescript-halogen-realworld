b02df8a204ebbfdc2c9cd3891a11f7716f62c3cb
Bartosz Nitka <niteria@gmail.com>
2025-04-30 17:16:14 +0200

    Use slimlock instead of node2nix

--- flake.nix
+++ flake.nix
@@ -4,42 +4,51 @@
     flake-utils.url = "github:numtide/flake-utils";
     mkSpagoDerivation.url = "github:jeslie0/mkSpagoDerivation";
     ps-overlay.url = "github:thomashoneyman/purescript-overlay";
+
+    slimlock.url = "github:thomashoneyman/slimlock";
+    slimlock.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, mkSpagoDerivation, ps-overlay }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs =
+    { self, nixpkgs, flake-utils, mkSpagoDerivation, ps-overlay, slimlock }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ mkSpagoDerivation.overlays.default
-                       ps-overlay.overlays.default ];
+          overlays = [
+            mkSpagoDerivation.overlays.default
+            ps-overlay.overlays.default
+            slimlock.overlays.default
+          ];
         };
-        nodeDependencies = (pkgs.callPackage ./node/default.nix { }).nodeDependencies;
-      in
-        {
-          packages.default =
-            pkgs.mkSpagoDerivation {
-              spagoYaml = ./spago.yaml;
-              spagoLock = ./spago.lock;
-              src = ./.;
-              nativeBuildInputs = [ pkgs.purs-unstable pkgs.spago-unstable pkgs.esbuild pkgs.purescript-language-server pkgs.node2nix];
-              version = "0.1.0";
-              buildPhase = ''
-                ln -s ${nodeDependencies}/lib/node_modules ./node_modules
-                spago bundle
-              '';
-              installPhase = ''
-                mkdir $out;
-                cp ${./dist/index.html} $out/index.html;
-                cp index.js $out/index.js;
-              '';
-              shellHook = ''
-                if [ ! -L "./node_modules" ]; then
-                  ln -s "${nodeDependencies}/lib/node_modules" ./node_modules
-                fi
-              '';
-            };
-        }
-    );
+        src = ./.;
+        modules = pkgs.slimlock.buildPackageLock { inherit src; };
+      in {
+        packages.default = pkgs.mkSpagoDerivation {
+          spagoYaml = ./spago.yaml;
+          spagoLock = ./spago.lock;
+          inherit src;
+          nativeBuildInputs = [
+            pkgs.purs-unstable
+            pkgs.spago-unstable
+            pkgs.esbuild
+            pkgs.purescript-language-server
+          ];
+          version = "0.1.0";
+          buildPhase = ''
+            ln -s ${modules}/js/node_modules ./node_modules
+            spago bundle
+          '';
+          installPhase = ''
+            mkdir $out;
+            cp ${./dist/index.html} $out/index.html;
+            cp index.js $out/index.js;
+          '';
+          shellHook = ''
+            if [ ! -L "./node_modules" ]; then
+              ln -s "${modules}/js/node_modules" ./node_modules
+            fi
+          '';
+        };
+      });
 }
